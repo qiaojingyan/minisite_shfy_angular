3c6551d17c307b17abe27dbf8be37ea7967c0ec0
qiaojingyan <1421640115@qq.com>
2016-11-22 16:43:41 +0800

    feedback v1

--- src/app/feedback/feedback.js
+++ src/app/feedback/feedback.js
@@ -2,8 +2,29 @@
 var feedbackModule = angular.module('feedbackModule', ['ngTouch']);
 
 feedbackModule.controller('feedbackCtrl', ['$scope', '$http', '$location', '$stateParams', 'Const', 'fyData', function($scope, $http, $location, $stateParams, Const, fyData) {
-	init();
-	function init(){
-		
-	}
+    init();
+
+    function init() {
+        $scope.myfeedback = {
+            IsPublic: true,
+            CategoryID: 'D1264B05-7FF9-4CB1-95FB-EE3106928C04'
+        };
+    }
+
+    $scope.cancelSubmit = function() {
+        window.history.back();
+    }
+
+    $scope.submit = function() {
+        $http({
+            method: 'POST',
+            url: Const.baseUrl + "/Feedback/SaveFeedback",
+            data: {
+                Token: fyData.user.token,
+                Feedback: $scope.myfeedback
+            }
+        }).success(function(res) {
+            console.log(res);
+        });
+    }
 }]);
