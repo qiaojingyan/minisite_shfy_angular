29aea63ee87a80afb58ea798a8bf3a1b5acd1466
qiaojingyan <1421640115@qq.com>
2016-11-25 18:53:34 +0800

    1

--- src/app/loading/loading.js
+++ src/app/loading/loading.js
@@ -5,9 +5,37 @@ var loadingModule = angular.module('loadingModule', ['ngTouch']);
 loadingModule.controller('loadingCtrl', ['$scope', '$http', '$location', '$stateParams', 'Const', 'fyData', function($scope, $http, $location, $stateParams, Const, fyData) {
     init();
 
+    function getCookie(cookie_name) {
+        var allcookies = document.cookie;
+        var cookie_pos = allcookies.indexOf(cookie_name); //索引的长度  
+        // 如果找到了索引，就代表cookie存在，  
+        // 反之，就说明不存在。 
+        if (cookie_pos != -1) {
+            // 把cookie_pos放在值的开始，只要给值加1即可。  
+            cookie_pos += cookie_name.length + 1; //这里我自己试过，容易出问题，所以请大家参考的时候自己好好研究一下。。。  
+            var cookie_end = allcookies.indexOf(";", cookie_pos);
+            if (cookie_end == -1) {
+                cookie_end = allcookies.length;
+            }
+            var value = unescape(allcookies.substring(cookie_pos, cookie_end)); //这里就可以得到你想要的cookie的值了。。。  
+        }
+        return value;
+    }
+
     function init() {
         var openid = $location.search()['openid'];
         var redirectPath = decodeURIComponent($stateParams.redirectUrl);
+
+        console.log(window.location.href);
+        if (openid == undefined || openid == null || openid == '') {
+            openid = getCookie('FXI.WeChat.OpenId');
+            if (openid == undefined || openid == null) {
+                window.location.href = 'http://wechat.fxigroup.com/wechat/oauth/index?param=' + encodeURIComponent('http://wechat.fxigroup.com/minisite/#/'+redirectPath);
+                return;
+            }
+        }
+
+
         $http({
             method: 'GET',
             url: Const.baseUrl + "/Token/GetToken",
